b07c2334d1d9e43f5dbd3de80fd8ca3454fb68dd
hyeonjuuu <janghyeonjuu@gmail.com>
2023-08-11 15:36:12 +0900

    CSS모듈 학습

--- src/pages/FilterableList.jsx
+++ src/pages/FilterableList.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import styleModule from '@/styles/FilterableList.module.css';
+
+console.log(styleModule);
 
 function FilterableList() {
   return (
@@ -6,7 +8,12 @@ function FilterableList() {
       <form>
         <div>
           <label htmlFor="todo"></label>
-          <input id="todo" type="text" placeholder="휴일에 할 일" />
+          <input
+            id="todo"
+            type="text"
+            placeholder="휴일에 할 일"
+            className={styleModule.primary}
+          />
         </div>
         <button type="submit"></button>
       </form>
