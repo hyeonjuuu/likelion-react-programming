f965bff4e3eea6b9ae91a50e643445d7e1727146
hyeonjuuu <janghyeonjuu@gmail.com>
2023-08-11 14:19:25 +0900

    이벤트 핸들링 적용

--- src/pages/Practice/parts/Controller.jsx
+++ src/pages/Practice/parts/Controller.jsx
@@ -8,9 +8,12 @@ function Controller() {
         direction="down"
         label="스크롤 다운"
         onClick={() => {
-          // side effect
+          // side effect 영역
+          // React 렌더링 과정과 상관 없이 여기에는 어떤 코드든 작성이 가능하다
+          // 왜냐하면 리얼월드에서 처리되기 때문.
+          // #이벤트 적용하기
           const practiceElement = getNode('.Practice');
-          console.log(practiceElement);
+          practiceElement.scroll({ top: 900, behavior: 'smooth' });
         }}
       />
 
@@ -19,6 +22,9 @@ function Controller() {
         label="스크롤 업"
         onClick={() => {
           console.log('go to up');
+          // #이벤트 적용하기
+          const practiceElement = getNode('.Practice');
+          practiceElement.scroll({ top: 0, behavior: 'smooth' });
         }}
       />
     </div>
