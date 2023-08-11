96f5f7f4116c2ee76dfc7d1b148a534e098b70fe
hyeonjuuu <janghyeonjuu@gmail.com>
2023-08-11 16:03:38 +0900

    React 5일차 학습

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
+import { env } from 'node:process';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     devSourcemap: true,
+    modules: {
+      generateScopedName: '[local]_[hash:base64:10]',
+    },
   },
   resolve: {
     alias: {
