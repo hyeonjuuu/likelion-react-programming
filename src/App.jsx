f5e3e15336922dd8eebae7b7c498eccee3c0acc9
hyeonjuuu <janghyeonjuu@gmail.com>
2023-08-18 15:02:32 +0900

    React Router 학습

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,46 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import RootLayout from './layout/RootLayout';
 
-const Home = () => <div>Home</div>;
-const Products = () => <div>Products</div>;
-const Contact = () => <div>Contact</div>;
+const Nav = () => (
+  <nav>
+    <ul>
+      <li>
+        <a href="/" className="hover:text-blue-400">
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="/products" className="hover:text-blue-400">
+          Products
+        </a>
+      </li>
+      <li>
+        <a href="/contact" className="hover:text-blue-400">
+          Contact
+        </a>
+      </li>
+    </ul>
+  </nav>
+);
+
+const Home = () => (
+  <div>
+    <Nav />
+    <h1 className="text-black text-emerald-500">Home</h1>
+  </div>
+);
+const Products = () => (
+  <div>
+    <Nav />
+    <h1 className="text-black text-emerald-500">Products</h1>
+  </div>
+);
+const Contact = () => (
+  <div>
+    <Nav />
+    <h1 className="text-black text-emerald-500">Contact</h1>
+  </div>
+);
 
 const router = createBrowserRouter([
   // Router Object
