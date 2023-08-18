95e6c8b6661b508deadb2dbe98f50596e40f105d
hyeonjuuu <janghyeonjuu@gmail.com>
2023-08-18 14:30:18 +0900

    router 적용해보기

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import { useState } from 'react';
+import { createBrowserRouter } from 'react-router-dom';
 import RootLayout from './layout/RootLayout';
-import LearnStateAndEffects from './pages/LearnStateAndEffects';
-import ProductList from './pages/ProductList';
+
+const Home = () => <div>Home</div>;
+const Products = () => <div>Products</div>;
+const Contact = () => <div>Contact</div>;
+
+// router  길 안내자
+// route 길
+const router = createBrowserRouter([
+  // Router Object = 객체
+
+  // 경로(path)에 어떤 요소(element <- <Component />)를 화면에 렌더링 할 건가요?
+  // { path: '/', element: <RootLayout /> },
+  { path: '/', element: <Home /> },
+  { path: '/products', element: <Products /> },
+  { path: '/contact', element: <Contact /> },
+]);
+
+console.log(router);
 
 function App() {
-  const [isVisible] = useState(true);
-
-  return (
-    <div className="App">
-      <RootLayout>
-        <LearnStateAndEffects />
-        {isVisible && <ProductList />}
-      </RootLayout>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
