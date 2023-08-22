049ac18a3785156de63d3b82632b8f7401192afe
hyeonjuuu <janghyeonjuu@gmail.com>
2023-08-22 11:28:31 +0900

    상품 수정 및 삭제 요청

--- src/hooks/useProductList.js
+++ src/hooks/useProductList.js
@@ -1,6 +1,7 @@
 import useFetchData from './useFetchData';
 
 const endpoint = `${import.meta.env.VITE_PB_API}/collections/products/records`;
+console.log(endpoint);
 
 function useProductList() {
   return useFetchData(endpoint);
